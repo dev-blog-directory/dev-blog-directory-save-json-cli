5de73a152b6bbca1b6b52803701284de09bd8eba
dailyrandomphoto <dailyrandomphoto@gmail.com>
2019-11-29 09:23:51 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const {resolve} = require('path');
+const {readJson} = require('fs-extra');
+const {save, saveAll} = require('dev-blog-directory-save');
+
+function main(filepath, options) {
+  return readJson(filepath)
+    .then(docs => {
+      if (!docs) {
+        return;
+      }
+
+      if (Array.isArray(docs)) {
+        docs = docs.filter(doc => doc && typeof doc === 'object');
+        return saveAll(docs, options);
+      }
+
+      return save(docs, options);
+    });
+}
+
+function exit(msg) {
+  if (msg) {
+    console.error('\n' + msg);
+
+    process.exit(1);
+  }
+
+  process.exit(0);
+}
+
+if (require.main === module) { // Called directly
+  const argv = require('minimist')(process.argv.slice(2), {boolean: ['merge']});
+  const filename = argv._[0];
+  const option = {merge: Boolean(argv.merge)};
+  if (!filename) {
+    exit('Usage: save-blog-json [--merge] JSONFILE');
+  }
+
+  const filepath = resolve(process.cwd(), filename);
+
+  console.log('json file name: ' + filename);
+  console.log('json full path: ' + filepath);
+  main(filepath, option)
+    .then(() => {
+      console.log('Done!');
+    })
+    .catch(exit);
+} else { // Required as a module
+  module.exports = main;
+}
---
+++ test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const fs = require('fs-extra');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const {expect} = chai;
+const main = require('dev-blog-directory-save-json-cli');
+
+describe('dev-blog-directory-save-json-cli', () => {
+  before(() => {
+    return fs.remove('./documents')
+      .then(() => {
+        console.log('remove ./documents success!');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  });
+
+  it('module should to be a function', () => {
+    expect(main).to.be.a('function');
+  });
+
+  it('should throw an error', () => {
+    return expect(main()).to.be.rejected;
+  });
+
+  it('should throw a SyntaxError', () => {
+    return expect(main('test.js')).to.be.rejectedWith(SyntaxError);
+  });
+
+  it('should not throw an error', () => {
+    return expect(main('test.json')).to.be.fulfilled;
+  });
+
+  it('duplicated url', () => {
+    return expect(main('test.json')).to.be.rejectedWith('Duplicated url');
+  });
+
+  it('with merge option', () => {
+    return expect(main('test.json', {merge: true})).to.be.fulfilled;
+  });
+});
